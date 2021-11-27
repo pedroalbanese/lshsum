b381bc65dead215777a9eeef1c35d698cde70b88
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-11-27 19:29:18 -0300

    Update main.go

--- cmd/lshmac/main.go
+++ cmd/lshmac/main.go
@@ -6,21 +6,39 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 
 	"github.com/RyuaNerin/go-krypto/lsh256"
 )
 
 func main() {
-	keyHex := flag.String("key", "", "Secret key")
+	keyHex := flag.String("k", "", "Secret key.")
+	target := flag.String("f", "", "Target file. ('-' for STDIN)")
 	flag.Parse()
+	if len(os.Args) < 2 {
+		fmt.Println("LSH-HMAC - ALBANESE Lab (c) 2020-2021\n")
+		fmt.Println("Usage of", os.Args[0]+":")
+		fmt.Printf("%s [-k <secret>] -f <file.ext>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 	key, err := hex.DecodeString(*keyHex)
 	if err != nil {
 		panic(err)
 	}
+	var file io.Reader
+	if *target == "-" {
+		file = os.Stdin
+	} else {
+		file, err = os.Open(*target)
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+	}
 	h := hmac.New(lsh256.New, key)
-	if _, err = io.Copy(h, os.Stdin); err != nil {
-		panic(err)
+	if _, err = io.Copy(h, file); err != nil {
+		log.Fatalf("failed opening file: %s", err)
 	}
 	fmt.Println("MAC:", hex.EncodeToString(h.Sum(nil)))
 }
